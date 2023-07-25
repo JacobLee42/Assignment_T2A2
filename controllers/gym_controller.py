6181882668544469c5c4284135e772e99da5f80c
JacobLee42 <14303@coderacademy.edu.au>
2023-07-25 10:32:24 +1000

    Nested comment controller inside gym controller

--- controllers/gym_controller.py
+++ controllers/gym_controller.py
@@ -3,8 +3,10 @@ from init import db
 from models.gym import Gym, gyms_schema, gym_schema
 from flask_jwt_extended import get_jwt_identity, jwt_required
 from models.comment import Comment, comment_schema, comments_schema
+from controllers.comment_controller import comments_bp
 
 gyms_bp = Blueprint('gyms', __name__, url_prefix='/gyms')
+gyms_bp.register_blueprint(comments_bp, url_prefix='/<int:gym_id>/comments')
 
 @gyms_bp.route('/')
 def get_all_gyms():
@@ -71,49 +73,3 @@ def update_one_gym(id):
     else:
         return {'error': f'Gym not found with id {id}'}, 404
     
-
-@gyms_bp.route('/<int:gym_id>/comments', methods=['POST'])
-@jwt_required()
-def create_comment(gym_id):
-    body_data = request.get_json()
-    stmt = db.select(Gym).filter_by(id=gym_id)
-    gym = db.session.scalar(stmt)
-    if gym:
-        comment = Comment(
-            message=body_data.get('message'),
-            user_id=get_jwt_identity(),
-            gym_id=gym.id
-        )
-        
-        db.session.add(comment)
-        db.session.commit()
-        return comment_schema.dump(comment), 201
-    else:
-        return {'error': f'Gym not found with id {gym_id}'}, 404
-    
-
-@gyms_bp.route('/<int:gym_id>/comments/<int:comment_id>', methods=['DELETE'])
-@jwt_required()
-def delete_comment(gym_id, comment_id):
-    stmt = db.select(Comment).filter_by(id=comment_id)
-    comment = db.session.scalar(stmt)
-    if comment:
-        db.session.delete(comment)
-        db.session.commit()
-        return {'message': f'Comment {comment.message} deleted successfully'}
-    else:
-        return {'error': f'Comment not found with id {comment_id}'}, 404
-    
-
-@gyms_bp.route('/<int:gym_id>/comments/<int:comment_id>', methods=['PUT', 'PATCH'])
-@jwt_required()
-def update_comment(gym_id, comment_id):
-    body_data = request.get_json()
-    stmt = db.select(Comment).filter_by(id=comment_id)
-    comment = db.session.scalar(stmt)
-    if comment:
-        comment.message = body_data.get('message') or comment.message
-        db.session.commit()
-        return comment_schema.dump(comment)
-    else:
-        return {'error': f'Comment not found with id {comment_id}'}, 404
